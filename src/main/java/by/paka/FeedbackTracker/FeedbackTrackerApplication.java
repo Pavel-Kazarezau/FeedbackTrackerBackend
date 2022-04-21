daf7ce3e04d5e14c4462cba0fb8cd01a21b634fe
paka <p.kazarezov@gmail.com>
2022-04-21 20:35:15 +0300

    Initial commit

---
+++ src/main/java/by/paka/FeedbackTracker/FeedbackTrackerApplication.java
@@ -0,0 +1,13 @@
+package by.paka.FeedbackTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/paka/FeedbackTracker/FeedbackTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.paka.FeedbackTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
