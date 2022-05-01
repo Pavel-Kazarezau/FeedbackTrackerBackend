3442314d254458bd95cbd757dc29fd8942f92e63
paka <p.kazarezov@gmail.com>
2022-05-01 15:48:57 +0300

    Add password encoder bean

--- src/main/java/by/paka/FeedbackTracker/FeedbackTrackerApplication.java
+++ src/main/java/by/paka/FeedbackTracker/FeedbackTrackerApplication.java
@@ -3,7 +3,10 @@ package by.paka.FeedbackTracker;
 import by.paka.FeedbackTracker.rest.config.RestConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @Import(RestConfig.class)
@@ -13,4 +16,8 @@ public class FeedbackTrackerApplication {
 		SpringApplication.run(FeedbackTrackerApplication.class, args);
 	}
 
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
