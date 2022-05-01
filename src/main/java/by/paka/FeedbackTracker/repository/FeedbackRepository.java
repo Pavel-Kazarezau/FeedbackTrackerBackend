d509af41142f1bf20233fe35b974130a1e1987d0
paka <p.kazarezov@gmail.com>
2022-05-01 15:37:01 +0300

    Configure JPA Repository for Feedback Entity

--- src/main/java/by/paka/FeedbackTracker/repository/FeedbackRepository.java
+++ src/main/java/by/paka/FeedbackTracker/repository/FeedbackRepository.java
@@ -1,11 +1,10 @@
 package by.paka.FeedbackTracker.repository;
 
 import by.paka.FeedbackTracker.model.FeedbackItem;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface FeedbackRepository {
-
-    void createFeedbackItem(FeedbackItem item);
-
-    FeedbackItem updateFeedbackItem(FeedbackItem item);
+public interface FeedbackRepository extends JpaRepository<FeedbackItem, Integer> {
+    @Override
+    FeedbackItem save(FeedbackItem feedbackItem);
 }
