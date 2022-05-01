6bcd150dea4c1a7f7ac5679df1c34dd1fc53ff3d
paka <p.kazarezov@gmail.com>
2022-05-01 15:30:27 +0300

    Change to CrudRepository in FeedbackRestRepository

--- src/main/java/by/paka/FeedbackTracker/rest/FeedbackRestRepository.java
+++ src/main/java/by/paka/FeedbackTracker/rest/FeedbackRestRepository.java
@@ -1,13 +1,13 @@
 package by.paka.FeedbackTracker.rest;
 
 import by.paka.FeedbackTracker.model.FeedbackItem;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
 
 @RepositoryRestResource(collectionResourceRel = "feedbacks", path = "feedbacks")
-public interface FeedbackRestRepository extends PagingAndSortingRepository<FeedbackItem, Integer> {
+public interface FeedbackRestRepository extends CrudRepository<FeedbackItem, Integer> {
 
     @Override
     Iterable<FeedbackItem> findAll();
