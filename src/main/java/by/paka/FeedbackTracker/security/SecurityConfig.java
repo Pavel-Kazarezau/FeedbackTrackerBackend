854daa85fe72f670578bdbb31a3a578b32a53058
paka <p.kazarezov@gmail.com>
2022-05-01 17:12:18 +0300

    Configure stab security config

--- src/main/java/by/paka/FeedbackTracker/security/SecurityConfig.java
+++ src/main/java/by/paka/FeedbackTracker/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import by.paka.FeedbackTracker.filter.PakaAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration @EnableWebSecurity @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -25,10 +27,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        final PakaAuthFilter authFilter = new PakaAuthFilter(authenticationManagerBean());
+        authFilter.setFilterProcessesUrl("/login");
+        http.addFilter(authFilter);
+
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        //http.authorizeRequests().antMatchers(HttpMethod.GET, "/feedbacks/**").hasAnyAuthority("ADMIN");
+        //http.authorizeRequests().antMatchers(HttpMethod.PUT, "/update/feedbacks").hasAnyAuthority("ADMIN");
         http.authorizeRequests().anyRequest().permitAll();
-        http.addFilter(new PakaAuthFilter(authenticationManagerBean()));
+        //http.authorizeRequests().anyRequest().authenticated();
+        //http.addFilterBefore(new PakaAuthFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
