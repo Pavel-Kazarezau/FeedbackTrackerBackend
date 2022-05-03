f9b474caccae6f85c3fdcba49555c52698a5b218
paka <p.kazarezov@gmail.com>
2022-05-03 15:16:57 +0300

    Add filtering rest

--- src/main/java/by/paka/FeedbackTracker/repository/FeedbackRepository.java
+++ src/main/java/by/paka/FeedbackTracker/repository/FeedbackRepository.java
@@ -3,6 +3,10 @@ package by.paka.FeedbackTracker.repository;
 import by.paka.FeedbackTracker.model.FeedbackItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface FeedbackRepository extends JpaRepository<FeedbackItem, Integer> {
+
+    List<FeedbackItem> getFeedbackItemByIsCompletedFalse();
 }
